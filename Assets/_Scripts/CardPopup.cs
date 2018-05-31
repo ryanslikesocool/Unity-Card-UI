e557886af2661500bc2f9a717481cc98146909fd
ryan boyer <ryanslikesocool@users.noreply.github.com>
2018-05-30 20:51:07 -0600

    added falling popup
    also learning to use GitHub Dektop

---
+++ Assets/_Scripts/CardPopup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPopup : MonoBehaviour {
+
+	public float rotationSpeed = 1f;
+	public float centeringSpeed = 4f;
+
+	private Vector3 startPosition;
+	private Rigidbody rbody;
+	private bool isFalling;
+	private Vector3 cardFallRotation;
+	private bool fallToZero;
+
+	void Start() {
+		rbody = GetComponent<Rigidbody>();
+		rbody.useGravity = false;
+		startPosition = transform.position;
+	}
+
+	void Update() {
+		if (isFalling) {
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(cardFallRotation), Time.deltaTime * rotationSpeed);
+		}
+
+///Everything in this fallToZero conditional is totally unnecessary.  I have it so the popup falls back nicely.		
+		if (fallToZero) {
+			transform.position = Vector3.Lerp(transform.position, startPosition, Time.deltaTime * centeringSpeed);
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.zero), Time.deltaTime * centeringSpeed);
+			if (Vector3.Distance(transform.position, startPosition) < 0.0001f) {
+				transform.position = startPosition;
+				fallToZero = false;
+			}
+		}
+
+///This is also probably unnecessary.
+		if (transform.position.y < -4) {
+			isFalling = false;
+			fallToZero = true;
+			rbody.useGravity = false;
+			rbody.velocity = Vector3.zero;
+			transform.position = new Vector3(0, 8, startPosition.z);			
+		}
+	}
+
+///A negative fallRotation will result in the card turning clockwise, while a positive fallRotation makes the card turn counterclockwise
+	public void CardFall(float fallRotation) {
+		fallToZero = false;
+		rbody.useGravity = true;
+		isFalling = true;
+		cardFallRotation = new Vector3(0, 0, fallRotation);
+	}
+}
--- Assets/_Scripts/CardStack.cs
+++ Assets/_Scripts/CardStack.cs
@@ -2,37 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardController : MonoBehaviour {
+public class CardStack : MonoBehaviour {
 
 	public float cardMoveSpeed = 8f;
 	public float buttonCooldownTime = 0.125f;
-	public int cardArrayOffset;
+	public int cardZMultiplier = 32;
+	public int usedCardXPos = 1280;
 	public GameObject[] cards;
-	public Vector3[] cardPositions;
-
+	
+	private int cardArrayOffset;
+	private Vector3[] cardPositions;
 	private bool canMove = true;
 	private int xPowerDifference;
 
 	void Start () {
+///I've found that 9 is a good number for this.  I wouldn't really recommend changing it, but go ahead if you want to.		
 		xPowerDifference = 9 - cards.Length;
 
 		cardPositions = new Vector3[cards.Length * 2 - 1];
-		
+
+///This for loop is for cards still in the stack.		
 		for (int i = cards.Length; i > -1; i--) {
 			if (i < cards.Length - 1) {
-				cardPositions[i] = new Vector3 (- Mathf.Pow(2, i + xPowerDifference) + cardPositions[i + 1].x, 0, 32 * Mathf.Abs(i + 1 - cards.Length));
+				cardPositions[i] = new Vector3 (- Mathf.Pow(2, i + xPowerDifference) + cardPositions[i + 1].x, 0, cardZMultiplier * Mathf.Abs(i + 1 - cards.Length));
 			} else {
 				cardPositions[i] = Vector3.zero;
 			}
 		}
 
+///This for loop is for cards outside of the stack.
 		for (int i = cards.Length; i < cardPositions.Length; i++) {
-			cardPositions[i] = new Vector3 (1152 + 4 * (i - cards.Length), 0, -2 + -2 * (i - cards.Length));
+			cardPositions[i] = new Vector3 (usedCardXPos + 4 * (i - cards.Length), 0, -2 + -2 * (i - cards.Length));
 		}
 	}
 	
 	void Update () {
 		if (canMove) {
+///Controls for the cards.  Kinda obvious imo.			
 			if (Input.GetAxisRaw("Horizontal") < 0 && cardArrayOffset > 0) {
 				cardArrayOffset--;
 				StartCoroutine(ButtonCooldown());
@@ -42,11 +48,13 @@ public class CardController : MonoBehaviour {
 			}
 		}
 
+///Moves the cards.  I know that none of my lerps are the "right way," but it looks much nicer.
 		for (int i = 0; i < cards.Length; i++) {
 			cards[i].transform.localPosition = Vector3.Lerp(cards[i].transform.localPosition, cardPositions[i + cardArrayOffset], Time.deltaTime * cardMoveSpeed);
 		}
 	}
 
+///Stops the cards from scrolling really fast.
 	IEnumerator ButtonCooldown() {
 		canMove = false;
 		yield return new WaitForSeconds(buttonCooldownTime);
