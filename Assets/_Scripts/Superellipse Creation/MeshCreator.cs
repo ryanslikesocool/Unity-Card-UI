02aac5562a1e4ff20358345d4d9eaa00279ecc69
ryan <ryanslikesocool@users.noreply.github.com>
2018-10-13 01:22:54 -0600

    Formatting changes

--- Assets/_Scripts/Superellipse Creation/MeshCreator.cs
+++ Assets/_Scripts/Superellipse Creation/MeshCreator.cs
@@ -15,7 +15,7 @@ public class MeshCreator : MonoBehaviour {
  
         // Create the Vector3 vertices
         Vector3[] vertices = new Vector3[vertices2D.Length];
-        for (int i=0; i<vertices.Length; i++) {
+        for (int i = 0; i < vertices.Length; i++) {
             vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
         }
  
